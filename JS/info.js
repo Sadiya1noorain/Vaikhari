123b9ced0512a330a0e9283bceee13f822196f18
Sadiya Noorain <53908125+Sadiya1noorain@users.noreply.github.com>
2022-06-15 07:43:23 +0530

    Update info.js

--- JS/info.js
+++ JS/info.js
@@ -13,17 +13,17 @@ $("#color-1").click(function () {
     $(".highlight-window").css('background-image', 'url("https://images.unsplash.com/photo-1622445275463-afa2ab738c34?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=774&q=80")');
 });
 $("#color-2").click(function () {
-    $(".highlight-window").css('background-image', 'url(./Pics/black.png)');
+    $(".highlight-window").css('background-image', 'url(../Pics/black.png)');
 });
 $("#color-3").click(function () {
-    $(".highlight-window").css('background-image', 'url(./Pics/red.png)');
+    $(".highlight-window").css('background-image', 'url(../Pics/red.png)');
 });
 $("#color-4").click(function () {
-    $(".highlight-window").css('background-image', 'url(./Pics/blue.png)');
+    $(".highlight-window").css('background-image', 'url(../Pics/blue.png)');
 });
 $("#color-5").click(function () {
-    $(".highlight-window").css('background-image', 'url(./Pics/yellow.png)');
+    $(".highlight-window").css('background-image', 'url(../Pics/yellow.png)');
 });
 $("#color-6").click(function () {
-    $(".highlight-window").css('background-image', 'url(./Pics/maroon.png)');
+    $(".highlight-window").css('background-image', 'url(../Pics/maroon.png)');
 });
